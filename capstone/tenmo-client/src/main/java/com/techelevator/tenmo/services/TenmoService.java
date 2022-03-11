39e1ba48ab13ffffe9546f43bf1242068940e26a
Nick Steinhouse <nick.steinhouse@gmail.com>
2022-03-10 20:20:34 -0500

    Added trasaction class, tenmoservice. added todos for them also

---
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/TenmoService.java
@@ -0,0 +1,13 @@
+package com.techelevator.tenmo.services;
+
+public class TenmoService {
+    //Needs two HttpEntity methods
+    //TODO (1)HttpEntity<Transaction> - Include both authtoken (credentials) and a transaction object to send to the server
+    //TODO (2)HttpEntity<Void> -Includes only authtoken (credentials)
+    //
+    //TODO Needs setAuthToken() method - Will receive auth token from App class currentUser.getToken()
+    //
+    //TODO Need methods for each server endpoint /balance, /send, /transactions, transactions/[id]
+
+
+}
